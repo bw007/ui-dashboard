24ef35289b52ca8459fc44387312d4011fac58d2
bw007 <abdulaziz00791@gmail.com>
2024-06-28 21:00:23 +0500

    Gulp full config

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
-const { src, dest, parallel, watch, series } = require("gulp");
-
+const { src, dest, watch, series } = require("gulp");
 const pug = require('gulp-pug');
 const scss = require("gulp-sass")(require("sass"));
 const concat = require("gulp-concat");
@@ -8,14 +7,17 @@ const browserSync = require("browser-sync").create();
 const autoprefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean');
 
-function pugToHTML() {
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
+
+async function pugToHTML() {
   return src("src/pug/pages/**/*.pug")
     .pipe(pug({ pretty: true }))
     .pipe(dest("dist/"))
     .pipe(browserSync.stream());
 }
 
-function css() {
+async function css() {
   return src("src/scss/style.scss")
     .pipe(autoprefixer())
     .pipe(concat("style.min.css"))
@@ -24,25 +26,27 @@ function css() {
     .pipe(browserSync.stream());
 }
 
-function js() {
-  return src("src/js/main.js")
-    .pipe(concat("main.min.js"))
-    .pipe(uglify())
-    .pipe(dest("src/js"))
+async function tsCompile() {
+  return tsProject.src()
+    .pipe(tsProject())
+    .js
+    .pipe(uglify())  // Minifikatsiya qilish
+    .pipe(dest("dist/js/"))
     .pipe(browserSync.stream());
 }
 
-function watching() {
-  watch(["src/scss/style.scss"], css);
-  watch(["src/js/main.js"], js);
-  watch(["src/*.html"]).on("change", browserSync.reload);
+async function watchFiles() {
+  watch(["src/scss/**/*.scss"], css);
+  watch(["src/ts/**/*.ts"], tsCompile);
+  watch(["src/pug/pages/**/*.pug"], pugToHTML);
 }
 
 function browsersync() {
   browserSync.init({
     server: {
-      baseDir: "src/",
+      baseDir: "dist/",
     },
+    port: 3000
   });
 }
 
@@ -51,11 +55,12 @@ function cleanDist() {
     .pipe(clean())
 }
 
-const build = series(cleanDist, css, js, pugToHTML);
-const watch = series(build, watching, browsersync);
+const build = series(cleanDist, css, pugToHTML, tsCompile);
+const watcher = series(build, watchFiles, browsersync);
 
 exports.css = css;
-exports.js = js;
-exports.watching = watching;
-exports.browsersync = browsersync;
-exports.default = watch;
+exports.tsCompile = tsCompile;
+exports.pugToHTML = pugToHTML;
+exports.cleanDist = cleanDist;
+exports.watch = watcher;
+exports.default = watcher;
--- src/js/main.js
+++
@@ -1,2 +0,0 @@
-const h1 = document.querySelector("h1");
-const btn = document.querySelector("button");
