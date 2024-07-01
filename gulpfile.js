0b02788a38a8abd7a9241ef3cb24f5c9aac0dd7f
bw007 <mr.abdulaziz00791@gmail.com>
2024-07-01 18:07:17 +0500

    Update gulpfile and optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 const { src, dest, watch, series } = require("gulp");
-const pug = require('gulp-pug');
+const pug = require("gulp-pug");
 const scss = require("gulp-sass")(require("sass"));
 const uglify = require("gulp-uglify-es").default;
 const browserSync = require("browser-sync").create();
-const autoprefixer = require('gulp-autoprefixer');
-const clean = require('gulp-clean');
+const autoprefixer = require("gulp-autoprefixer");
+const clean = require("gulp-clean");
 const cssbeautify = require("gulp-cssbeautify");
 const cssnano = require("gulp-cssnano");
 const imagemin = require("gulp-imagemin");
@@ -12,53 +12,75 @@ const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const rigger = require("gulp-rigger");
 
-const ts = require('gulp-typescript');
-const tsProject = ts.createProject('tsconfig.json');
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
+
+const path = {
+  src: {
+    pug: "src/pug/pages/**/*.pug",
+    scss: "src/scss/style.scss",
+    ts: "src/ts/**/*.ts",
+    img: "src/imgs/**/*.{jpg,png,svg,gif,ico}",
+  },
+  watch: {
+    pug: "src/pug/**/*.pug",
+    scss: "src/scss/**/*.scss",
+    ts: "src/ts/**/*.ts",
+    img: "src/imgs/**/*.{jpg,png,svg,gif,ico}",
+  },
+  build: {
+    html: "dist/",
+    css: "dist/css/",
+    js: "dist/js/",
+    img: "dist/imgs/",
+  },
+};
 
 async function pugToHTML() {
-  return src("src/pug/pages/**/*.pug")
+  return src(path.src.pug)
     .pipe(pug({ pretty: true }))
-    .pipe(dest("dist/"))
+    .pipe(dest(path.build.html))
     .pipe(browserSync.stream());
 }
 
-async function css() {
-  return src("src/scss/style.scss", { base: "src/scss/" })
+async function scssToCss() {
+  return src(path.src.scss, { base: "src/scss/" })
     .pipe(plumber())
     .pipe(scss())
     .pipe(autoprefixer())
     .pipe(cssbeautify())
-    .pipe(dest("dist/css/"))
+    .pipe(dest(path.build.css))
     .pipe(cssnano())
     .pipe(rename({ suffix: ".min", extname: ".css" }))
-    .pipe(dest("dist/css/"))
+    .pipe(dest(path.build.css))
     .pipe(browserSync.stream());
 }
 
 async function tsCompile() {
-  return tsProject.src()
+  return tsProject
+    .src()
     .pipe(tsProject())
     .js
     .pipe(plumber())
     .pipe(rigger())
-    .pipe(dest("dist/js/"))
+    .pipe(dest(path.build.js))
     .pipe(uglify())
     .pipe(rename({ suffix: ".min", extname: ".js" }))
-    .pipe(dest("dist/js/"))
+    .pipe(dest(path.build.js))
     .pipe(browserSync.stream());
 }
 
 async function images() {
-  return src("src/imgs/**/*.{jpg,png,svg,gif,ico}")
+  return src(path.src.img)
     .pipe(imagemin())
-    .pipe(dest("dist/imgs/"));
+    .pipe(dest(path.build.img));
 }
 
 async function watchFiles() {
-  watch(["src/scss/**/*.scss"], css);
-  watch(["src/ts/**/*.ts"], tsCompile);
-  watch(["src/pug/**/*.pug"], pugToHTML);
-  watch(["src/imgs/**/*.{jpg,png,svg,gif,ico}"], images);
+  watch([path.watch.scss], scssToCss);
+  watch([path.watch.ts], tsCompile);
+  watch([path.watch.pug], pugToHTML);
+  watch([path.watch.img], images);
 }
 
 function browsersync() {
@@ -66,19 +88,19 @@ function browsersync() {
     server: {
       baseDir: "dist/",
     },
-    port: 3000
+    port: 3000,
   });
 }
 
 function cleanDist() {
   return src("dist", { allowEmpty: true })
-    .pipe(clean())
+    .pipe(clean());
 }
 
-const build = series(cleanDist, css, pugToHTML, images, tsCompile);
+const build = series(cleanDist, scssToCss, pugToHTML, images, tsCompile);
 const watching = series(build, watchFiles, browsersync);
 
-exports.css = css;
+exports.scssToCss = scssToCss;
 exports.tsCompile = tsCompile;
 exports.pugToHTML = pugToHTML;
 exports.images = images;
